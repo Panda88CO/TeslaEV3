2bc82e01ca8e9d7f4091d2eb6ec67cb9322e96d7
Panda88CO <christian.olgaard@gmail.com>
2025-03-13 14:24:22 -0700

    Update TeslaEV3main.py

--- TeslaEV3main.py
+++ TeslaEV3main.py
@@ -554,13 +554,13 @@ class TeslaEVController(udi_interface.Node):
             location = self.TEVcloud.teslaEV_GetLocation(self.EVid)
             logging.debug(f'teslaEV_GetLocation {location}')
             if location['longitude']:
-                logging.debug('GV17: {}'.format(round(location['longitude'], 3)))
+                logging.debug('GV17: {}'.format(round(location['longitude'], 2)))
                 self.EV_setDriver('GV17', round(location['longitude'], 3), 56)
             else:
                 logging.debug(f'GV17: NONE')
                 self.EV_setDriver('GV17', None, 25)
             if location['latitude']:
-                logging.debug('GV18: {}'.format(round(location['latitude'], 3)))
+                logging.debug('GV18: {}'.format(round(location['latitude'], 2)))
                 self.EV_setDriver('GV18', round(location['latitude'], 3), 56)
             else:
                 logging.debug('GV18: NONE')
