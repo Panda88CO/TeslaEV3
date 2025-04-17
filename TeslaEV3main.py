59a0370938673455e814b6fa20589b89a137222a
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2025-04-16 18:55:54 -0700

    0.0.36

--- TeslaEV3main.py
+++ TeslaEV3main.py
@@ -20,7 +20,7 @@ from TeslaEVPwrShareNode import teslaEV_PwrShareNode
 from TeslaEVOauth import teslaAccess
 
 
-VERSION = '0.0.35'
+VERSION = '0.0.36'
 
 class TeslaEVController(udi_interface.Node):
     from  udiLib import node_queue, command_res2ISY, code2ISY, wait_for_node_done,tempUnitAdjust, display2ISY, sentry2ISY, setDriverTemp, cond2ISY,  mask2key, heartbeat, state2ISY, sync_state2ISY, bool2ISY, online2ISY, EV_setDriver, openClose2ISY
