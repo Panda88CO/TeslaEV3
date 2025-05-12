739611eba480ffd606b46e0b48e72e5d46979acc
Panda88CO <christian.olgaard@gmail.com>
2025-05-12 10:00:35 -0700

    Update TeslaEV3main.py

--- TeslaEV3main.py
+++ TeslaEV3main.py
@@ -60,7 +60,6 @@ class TeslaEVController(udi_interface.Node):
         self.customParameters = Custom(self.poly, 'customparams')
         self.portalData = Custom(self.poly, 'customNSdata')
         self.Notices = Custom(polyglot, 'notices')
-        self.ISYforced = False
         self.initialized = False
         self.STATE_UPDATE_MIN = 15
         self.primary = primary
