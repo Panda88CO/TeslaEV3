469d35a5197e91257ae2462138787a618a5afae7
Christian Olgaard <colgaard@litepoint.com>
2025-05-30 21:15:51 -0700

    Update TeslaEV3main.py

--- TeslaEV3main.py
+++ TeslaEV3main.py
@@ -23,7 +23,7 @@ from TeslaEVPwrShareNode import teslaEV_PwrShareNode
 from TeslaEVapi import teslaAccess
 
 
-VERSION = '0.1.41'
+VERSION = '0.1.42'
 
 class TeslaEVController(udi_interface.Node):
     from  udiLib import node_queue, command_res2ISY, code2ISY, wait_for_node_done,tempUnitAdjust, display2ISY, sentry2ISY, setDriverTemp, cond2ISY,  mask2key, heartbeat, state2ISY, sync_state2ISY, bool2ISY, online2ISY, EV_setDriver, openClose2ISY
@@ -244,7 +244,7 @@ class TeslaEVController(udi_interface.Node):
         logging.debug('customParamsHandler finish ')
         
     def process_message(self):
-        logging.debug('Stating proccess_mnessage thread')
+        logging.debug('Stating proccess_message thread')
         while True:
             try:
       
@@ -308,6 +308,7 @@ class TeslaEVController(udi_interface.Node):
                 qsize = self.messageQueue.qsize()
                 while qsize != 0:
                     time.sleep(1)
+                    qsize = self.messageQueue.qsize()
                 #if self.subnodesReady():            
                 #    self.update_all_drivers()
                 '''
