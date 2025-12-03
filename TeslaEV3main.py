45b84fd324d29b615a6ab58f99cff99f95788033
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2025-12-03 13:17:56 -0800

    test

--- TeslaEV3main.py
+++ TeslaEV3main.py
@@ -432,9 +432,10 @@ class TeslaEVController(udi_interface.Node):
             self.poly.Notices['NOTONLINE']=f'{EVname} appears offline - cannot continue without EV being online'
         # force creation of new config - assume this will enable retransmit of all data 
         self.poly.Notices['subscribe1'] = 'Subscribing to datastream from EV'
-        if not self.tesla_api.teslaEV_streaming_check_certificate_update(self.EVid, True): #We need to update streaming server credentials
-            logging.info('')
-            self.poly.Notices['SYNC']=f'{EVname} ERROR failed to connect to streaming server - EV may be too old'
+        conf_code, conf_res = self.tesla_api.teslaEV_streaming_check_certificate_update(self.EVid, True)
+        if not conf_code: #We need to update streaming server credentials
+            logging.error(f'{EVname} ERROR failed to connect to streaming server - {conf_res}')
+            self.poly.Notices['SYNC']=f'{EVname} ERROR failed to connect to streaming server - {conf_res}'
             #self.stop()
             sys.exit()
         
